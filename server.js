2bd393d128ffe2e1a928f4f35238550a71b780f8
oussama_messaoudi <oussamamessaoudi94@gmail.com>
2021-10-07 12:15:09 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`server is work $(port)`));
